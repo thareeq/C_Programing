07ff11eabd0017721e12932a95bf1a8414c7f8f4
thareeq <thareeq123@gmail.com>
2025-09-23 14:35:12 +0530

    single linkedlist

---
+++ linked_list/single_linked_list/single_linked_list.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node* next;
+}Node;
+
+Node* head = NULL;
+
+Node* create_new_node(int data){
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    if(new_node==NULL){
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+    new_node->data=data;
+    new_node->next=NULL;
+    return new_node;
+}
+
+void add_element_to_tail(int data){
+    Node* new_node = create_new_node(data);
+    if(head==NULL){
+        head=new_node;
+        return;
+    }
+    Node *temp = head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next = new_node;
+}
+
+void add_element_to_head(int data){
+    Node *new_node = create_new_node(data);
+    if(head==NULL){
+        head=new_node;
+        return;
+    }
+    new_node->next=head;
+    head=new_node;
+}
+/**
+ * Insert element at given position
+ * @param position (1-based index)
+ * @param data value to insert
+ * @return 1 if success, 0 if failure
+ */
+int add_element_at_pos(int position, int data){
+    if(position<1){
+        fprintf(stderr, "Invalid position\n");
+        return 0;
+    }
+    if(position==1){
+        add_element_to_head(data);
+        return 1;
+    }
+    Node* temp = head;
+    for(int i=1;temp!=NULL && i<(position-1);i++)
+    {
+        temp=temp->next;
+    }
+    if(temp==NULL){
+        fprintf(stderr,"Out of index\n");
+        return 0;
+    }
+    Node* new_node = create_new_node(data);
+    new_node->next=temp->next;
+    temp->next=new_node;
+    return 1;
+}
+
+void delete_head(){
+    if(head==NULL) return;
+    Node* temp=head;
+    head=head->next;
+    free(temp);
+}
+
+void delete_tail(){
+    if(head==NULL)return;
+    Node *temp = head;
+    while(temp->next->next!=NULL){
+        temp=temp->next;
+    }
+    free(temp->next);
+    temp->next=NULL;
+}
+
+int delete_at_position(int position){
+    if(position<1){
+        fprintf(stderr,"Invalid Position\n");
+        return 0;
+    }
+    if(position==1){
+        delete_head();
+        return 1;
+    }
+    Node *temp=head;
+    for(int i=1; temp!=NULL && i<position-1;i++){
+        temp=temp->next;
+    }
+    if(temp==NULL){
+        fprintf(stderr,"DEL: Out of Index\n");
+        return 0;
+    }
+    printf("ELement readched %d\n",temp->data);
+    Node* next = temp->next->next;
+    free(temp->next);
+    temp->next=next;
+    return 1;
+}
+
+void display(){
+    Node *temp = head;
+    while(temp!=NULL){
+        printf("%d",temp->data);
+        if(temp->next!=NULL) printf("->");
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+int main(){
+    add_element_to_tail(10);
+    add_element_to_tail(20);
+    add_element_to_tail(30);
+    display();
+    add_element_to_head(9);
+    add_element_to_head(8);
+    display();
+    add_element_at_pos(0,2);
+    add_element_at_pos(0,1);
+    add_element_at_pos(1,5);
+    add_element_at_pos(2,6);
+    add_element_at_pos(7,28);
+    add_element_at_pos(9,31);
+    add_element_at_pos(11,32);
+    display();
+    delete_head();
+    delete_tail();
+    display();
+    printf("Delete at position: \n");
+    delete_at_position(8);
+    delete_at_position(7);
+    delete_at_position(1);
+    display();
+    delete_at_position(3);
+    display();
+
+}
